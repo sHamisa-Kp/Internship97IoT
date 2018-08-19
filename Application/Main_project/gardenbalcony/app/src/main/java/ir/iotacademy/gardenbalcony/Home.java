3baca4532d48cfe032a5c664ed9f8ce265ac3628
nazgolnikravesh <nazgolnikravesh@yahoo.com>
2018-08-19 15:59:12 +0430

    first level code 2 pages(login-home)not compeleted

---
+++ Application/Main_project/gardenbalcony/app/src/main/java/ir/iotacademy/gardenbalcony/Home.java
@@ -0,0 +1,41 @@
+package ir.iotacademy.gardenbalcony;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Home extends AppCompatActivity {
+
+    ImageButton clouds;
+    ImageButton drop;
+    ImageButton grass;
+    ImageButton sun;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        clouds = (ImageButton) findViewById(R.id.imagebutton3);
+        grass = (ImageButton) findViewById(R.id.imagebutton1);
+        drop = (ImageButton) findViewById(R.id.imagebutton4);
+        sun  = (ImageButton) findViewById(R.id.imagebutton2);
+
+        clouds.setOnLongClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+             messagebox.
+
+            }
+
+
+        });
+
+    }
+}
