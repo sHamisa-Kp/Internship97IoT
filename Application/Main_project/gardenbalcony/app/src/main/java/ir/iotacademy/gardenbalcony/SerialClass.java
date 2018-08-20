0dc13e3d3b38d2ed5142d66adb65f17ada7d7a16
nazgolnikravesh <nazgolnikravesh@yahoo.com>
2018-08-20 14:12:08 +0430

    another changes in clouds and sun

---
+++ Application/Main_project/gardenbalcony/app/src/main/java/ir/iotacademy/gardenbalcony/SerialClass.java
@@ -0,0 +1,13 @@
+package ir.iotacademy.gardenbalcony;
+
+/**
+ * Created by Nazgol on 8/19/2018.
+ */
+
+class SerialClass {
+    public static String Temprature ;
+    public static String Humedity ;
+    public static String Light ;
+    public static String Moisture ;
+
+}
