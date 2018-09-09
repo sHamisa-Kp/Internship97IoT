7daf4422192024f0175e2453c5395e85e10b31f6
minaashokouhi <minaashokouhi@gmail.com>
2018-09-09 17:03:06 +0430

    add liquide gauge into waterLevel Tile

--- Platform/finalDashboard/js/waterLevel.js
+++ Platform/finalDashboard/js/waterLevel.js
@@ -1,9 +1,9 @@
 const waterLevelChannel = {
-'	WL': [{'id': '742', 'apiKey': 'WGWJ660WN7V9394D'}]
+	'WL': [{'id': '742', 'apiKey': 'WGWJ660WN7V9394D'}]
 };
 
 const waterLevelErrorValue = {
-	'WL': {'min': 20, 'max': 30}
+	'WL': {'min': 20, 'max': 99}
 };
 
 function waterLevelHttpGetAsync(theUrl, callback, i) {
@@ -13,8 +13,8 @@ function waterLevelHttpGetAsync(theUrl, callback, i) {
             // callback(xmlHttp.responseText);
             let text = JSON.parse(xmlHttp.responseText).field1;
             let num = parseInt(text);
-            waterLeveArray[i] = num;
-            if(num < waterLeveErrorValue.H.min || num  > waterLeveErrorValue.H.max) {
+            waterLevelArray[i] = num;
+            if(num < waterLevelErrorValue.WL.min || num  > waterLevelErrorValue.WL.max) {
             	waterLevelError[i] = true;
             } else {
             	waterLevelError[i] = false;
@@ -30,6 +30,38 @@ function waterLevelUpdateThePage() {
 		waterLevelHttpGetAsync("http://thingtalk.ir/channels/" + waterLevelChannel.WL[i].id + 
 			"/feeds/last.json?key=" + waterLevelChannel.WL[i].apiKey, function(){}, i);
 	}
+}
+
+function waterLevelCalculateAverage() {
+    let sum = 0;
+    humidityArray.forEach(function(elem) {
+        sum += elem;
+    });
+    let average = sum / humidityArray.length;
+    console.log(average);
+    console.log(humidityArray.length);
+    console.log(sum);
+
+    updateWaterLevelTile(average);
+}
+
+function updateWaterLevelTile(average) {
+
+    // var guageValue = document.querySelector("#fillgauge2");
+    // guageValue.values = average;
+
+    waterLevelErrorImage = document.getElementById('waterLevelErrorImage');
+    let thereIsAtLeastOneDanger = false;
+    waterLevelError.forEach(function(elem) {
+        if(elem) {
+            thereIsAtLeastOneDanger = true;
+        }
+    });
+    if(thereIsAtLeastOneDanger) {
+        waterLevelErrorImage.style.visibility = 'visible';
+    } else {
+        waterLevelErrorImage.style.visibility = 'hidden';
+    }
 }
 
 let waterLevelArray = [];
