9847df76d7d3881feb1443060a4803c6ec03aad6
nafiseabdollahi <n.a.bme.1995@gmail.com>
2018-09-17 08:43:16 -0400

    add view4

--- Platform/proofOfConcept/dashboardApplication/app.js
+++ Platform/proofOfConcept/dashboardApplication/app.js
@@ -3,11 +3,12 @@ const request = require('request');
 
 const tipBoardAPIKey = 'a500a57d8d1b4a5b9bb88c2d35108352';
 const channel = {
-    'T': [{'id': '629', 'apiKey': 'G7KHR97UPN9OC5AC'}],
-    'H': [{'id': '669', 'apiKey': '7TPW8OQOGN1EMURD'}],
-    'SM': [{'id': '670', 'apiKey': 'LUJ9D21E177HESAW'}],
-    'PS': [{'id': '671', 'apiKey': 'XAKAVEUUJQ9GZGMT'}],
-    'PhR': [{'id': '672', 'apiKey': 'B1JQYWFKX2PCRBYF'}]
+
+    'SM': [{'id': '670', 'apiKey': 'LUJ9D21E177HESAW'},{'id': '700', 'apiKey': 'Q2JH4OBED4QQAA74'},
+    {'id': '701', 'apiKey': 'P6LY6LV7CPYYSJUP'},{'id': '702', 'apiKey': 'PVWFQI4XLXLWL9DL'},{'id': '703', 'apiKey': '6W8DYBXG0HDA141O'},
+    {'id': '704', 'apiKey': 'HZ9VM1PH1Q6LQSLZ'},{'id': '705', 'apiKey': 'RUB06UUHPX0K4DDS'},{'id': '706', 'apiKey': 'WQFB2JIGRVXDIAR4'}]
+
+
 };
 const updateInterval = 3000; // ms
 
@@ -63,34 +64,49 @@ function getValueRequestToThingTalk(channel, key, key2, title, description) {
 }   
 
 function updateCharts() {
-    let data = {"subtitle": "Soil Moisture", "description": "", "series_list": []};
-    let formData = {'tile': "line_chart", 'key': "soilMoistureChart", 'data': ''};
+    let data = {"subtitle": "Soil Moisture 0", "description": "", "series_list": []};
+    let formData = {'tile': "line_chart", 'key': "SoilMoistureChart0", 'data': ''};
     getChartRequestToThingTalk(channel.SM[0].id, channel.SM[0].apiKey, data, formData);
 
-    data = {"subtitle": "Pump Status", "description": "", "series_list": []};
-    formData = {'tile': "line_chart", 'key': "pumpStatusChart", 'data': ''};
-    getChartRequestToThingTalk(channel.PS[0].id, channel.PS[0].apiKey, data, formData);
+    data = {"subtitle": "Soil Moisture 1", "description": "", "series_list": []};
+    formData = {'tile': "line_chart", 'key': "SoilMoistureChart1", 'data': ''};
+    getChartRequestToThingTalk(channel.SM[1].id, channel.SM[1].apiKey, data, formData);
 
-    data = {"subtitle": "Humidity", "description": "", "series_list": []};
-    formData = {'tile': "line_chart", 'key': "humidityChart", 'data': ''};
-    getChartRequestToThingTalk(channel.H[0].id, channel.H[0].apiKey, data, formData);
+    data = {"subtitle": "Soil Moisture 2", "description": "", "series_list": []};
+    formData = {'tile': "line_chart", 'key': "SoilMoistureChart2", 'data': ''};
+    getChartRequestToThingTalk(channel.SM[2].id, channel.SM[2].apiKey, data, formData);
 
-    data = {"subtitle": "Temperature", "description": "", "series_list": []};
-    formData = {'tile': "line_chart", 'key': "temperatureChart", 'data': ''};
-    getChartRequestToThingTalk(channel.T[0].id, channel.T[0].apiKey, data, formData);
+    data = {"subtitle": "Soil Moisture 3", "description": "", "series_list": []};
+    formData = {'tile': "line_chart", 'key': "SoilMoistureChart3", 'data': ''};
+    getChartRequestToThingTalk(channel.SM[3].id, channel.SM[3].apiKey, data, formData);
 
-    data = {"subtitle": "Photo Resistor", "description": "", "series_list": []};
-    formData = {'tile': "line_chart", 'key': "photoResistorChart", 'data': ''};
-    getChartRequestToThingTalk(channel.PhR[0].id, channel.PhR[0].apiKey, data, formData);
+    data = {"subtitle": "Soil Moisture 4", "description": "", "series_list": []};
+    formData = {'tile': "line_chart", 'key': "SoilMoistureChart4", 'data': ''};
+    getChartRequestToThingTalk(channel.SM[4].id, channel.SM[4].apiKey, data, formData);
+
+    data = {"subtitle": "Soil Moisture 5", "description": "", "series_list": []};
+    formData = {'tile': "line_chart", 'key': "SoilMoistureChart5", 'data': ''};
+    getChartRequestToThingTalk(channel.SM[5].id, channel.SM[5].apiKey, data, formData);
+    
+    data = {"subtitle": "Soil Moisture 6", "description": "", "series_list": []};
+    formData = {'tile': "line_chart", 'key': "SoilMoistureChart6", 'data': ''};
+    getChartRequestToThingTalk(channel.SM[6].id, channel.SM[6].apiKey, data, formData);
+    
+    data = {"subtitle": "Soil Moisture 7", "description": "", "series_list": []};
+    formData = {'tile': "line_chart", 'key': "SoilMoistureChart7", 'data': ''};
+    getChartRequestToThingTalk(channel.SM[7].id, channel.SM[7].apiKey, data, formData);
     
 }
 
 function updateJustValues() {
-    getValueRequestToThingTalk(channel.T[0].id, channel.T[0].apiKey, 'temperature', 'Temperature', "");
-    getValueRequestToThingTalk(channel.SM[0].id, channel.SM[0].apiKey, 'soilMoisture', 'SoilMoisture', "");
-    getValueRequestToThingTalk(channel.PhR[0].id, channel.PhR[0].apiKey, 'photoResistor', 'PhotoResistor', "");
-    getValueRequestToThingTalk(channel.H[0].id, channel.H[0].apiKey, 'humidity', 'Humidity', "");
-    //getValueRequestToThingTalk(channel.PS[0].id, channel.PS[0].apiKey, 'pumpStatus', 'PumpStatus', "");
+     getValueRequestToThingTalk(channel.SM[0].id, channel.SM[0].apiKey, 'SoilMoistureChart0', 'Soil Moisture Chart 0', "");
+     getValueRequestToThingTalk(channel.SM[1].id, channel.SM[1].apiKey, 'SoilMoistureChart1', 'Soil Moisture Chart 1', "");
+     getValueRequestToThingTalk(channel.SM[2].id, channel.SM[2].apiKey, 'SoilMoistureChart2', 'Soil Moisture Chart 2', "");
+     getValueRequestToThingTalk(channel.SM[3].id, channel.SM[3].apiKey, 'SoilMoistureChart3', 'Soil Moisture Chart 3', "");
+     getValueRequestToThingTalk(channel.SM[4].id, channel.SM[4].apiKey, 'SoilMoistureChart4', 'Soil Moisture Chart 4', "");
+     getValueRequestToThingTalk(channel.SM[5].id, channel.SM[5].apiKey, 'SoilMoistureChart5', 'Soil Moisture Chart 5', "");
+     getValueRequestToThingTalk(channel.SM[6].id, channel.SM[6].apiKey, 'SoilMoistureChart6', 'Soil Moisture Chart 6', "");
+     getValueRequestToThingTalk(channel.SM[7].id, channel.SM[7].apiKey, 'SoilMoistureChart7', 'Soil Moisture Chart 7', "");
 }
 
 
