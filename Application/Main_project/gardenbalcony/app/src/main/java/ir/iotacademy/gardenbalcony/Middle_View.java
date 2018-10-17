cdc63b6a1bb16aa5e227c6cbbd76781e802560c0
nazgolnikravesh <nazgolnikravesh@yahoo.com>
2018-10-17 16:35:45 +0330

    added setgetdata java class

---
+++ Application/Main_project/gardenbalcony/app/src/main/java/ir/iotacademy/gardenbalcony/Middle_View.java
@@ -0,0 +1,13 @@
+package ir.iotacademy.gardenbalcony;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Middle_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_middle__view);
+    }
+}
