74fec6cb0e99a40c0afb35dabe64f29eb2b5ce55
M.MeHdi Naseri <m.n4seri@gmail.com>
2018-10-17 17:55:29 +0330

    ADD automateTasks to raspberryMainController.py

--- Hardware/Raspberry/finalProject/raspberryMainController/mainController.py
+++ Hardware/Raspberry/finalProject/raspberryMainController/mainController.py
@@ -8,81 +8,28 @@ import datetime
 
 app = Flask(__name__)
 
+currentValuesOfVariables = {
+    'PS': [None, None, None],
+    'LBS': [None, None],
+    'TS': [None],
+    'SM': [None for i in range(18)],
+    'T': [None, None],
+    'H': [None, None],
+    'FH': [None, None],
+    'PH': [None for i in range(18)],
+    'WL': [None],
+    'PhR': [None, None],
+    'GS': [None, None],
+    'MD': [None, None],
+    'WM': [None]
+}
 
-@app.route("/actuators/<command>")
-def pumpControl(command):
-    if not command.startswith('set'):
-        return
-
-    dataTypes = ['PS', 'LBS', 'TS']
-
-    for dT in dataTypes:
-        if command[3:].startswith(dT):
-            if command[len(dT) + 1 + 3:] == 'ON' or command[len(dT) + 1 + 3:] == 'OFF':
-                if int(command[len(dT) + 3]) < len(apiKeys[dT]):
-                    ser.write(command.encode('ascii'))
-                    print("This Text has been sent to Arduino: ", command)
-                    return command
-
-    return "NOT FOUND", 404
-
-
-def zigbeeDataToString(inputBin):
-    inputStr = inputBin.decode('ascii')
-    return inputStr
-
-
-def stringToData(inputStr, dataType):
-    nodeNumber = inputStr[len(dataType): inputStr.find('=') - 1]
-    return {'dataType': dataType, 'data': inputStr[len(dataType + nodeNumber + ' = '):-2], 'nodeNumber': nodeNumber}
-
-
-def stringToDataHandler(inputStr):
-    dataTypes = ['SM', 'T', 'H', 'FH', 'PH', 'WL', 'PhR', 'PS', 'LBS', 'G', 'TS', 'MD', 'WM']
-
-    for dT in dataTypes:
-        if inputStr.startswith(dT):
-            return stringToData(inputStr, dT)
-
-    print("ERROR: Wrong data format.")
-
-
-def sendData(inputData):
-    try:
-        payload = {'api_key': apiKeys[inputData["dataType"]][int(inputData["nodeNumber"])]['apiKey'],
-                   'field1': inputData["data"]}
-        r = requests.post("http://thingtalk.ir/update", data=payload)
-        print("Data sent! ")
-        print(r.status_code, r.reason)
-        print("r.text: " + r.text)
-        print("--------------------------------------------------")
-        if r.status_code == 200 and r.text != '0':
-            return True
-        return False
-    except:
-        print("Connection Error... Thingtalk is kidding us :(")
-        return False
-
-
-def serialReadThread():
-    print("Now Raspberry pi is ready :)")
-    while True:
-        if ser.inWaiting() > 0:
-            print(datetime.datetime.now())
-            inputBin = ser.readline()
-
-            inputStr = zigbeeDataToString(inputBin)
-            print("inputStr: " + inputStr)
-            inputData = stringToDataHandler(inputStr)
-            print("inputData: ")
-            print(inputData)
-            sendData(inputData)
-
-        ### TEST THREAD WORKING ###
-        # print("Enter 1 or 0:")
-        # num = input()
-        # ser.write(num.encode('ascii'))
-
+thresholdsOfSensors = {
+    'H': {'min': 40, 'max': 60},
+    'PhR': {'min': 300, 'max': 350},
+    'SM': {'minV': 50, 'maxV': 80, 'minF': 50, 'maxF': 80},
+    'WL': {'min': 0.1, 'max': 0.9}
+}
 
 apiKeys = {
     # SoilMoisture (Vegetables: 0-7, Flowers: 8-17)
@@ -175,6 +122,187 @@ ser = serial.Serial(
     bytesize=serial.EIGHTBITS,
     timeout=1
 )
+
+
+@app.route("/actuators/<command>")
+def pumpControl(command):
+    if not command.startswith('set'):
+        return
+
+    dataTypes = ['PS', 'LBS', 'TS']
+
+    for dT in dataTypes:
+        if command[3:].startswith(dT):
+            if command[len(dT) + 1 + 3:] == 'ON' or command[len(dT) + 1 + 3:] == 'OFF':
+                if int(command[len(dT) + 3]) < len(apiKeys[dT]):
+                    ser.write((command + "\n").encode('ascii'))
+                    print("This Text has been sent to Arduino: ", command)
+                    return command
+
+    return "NOT FOUND", 404
+
+
+def zigbeeDataToString(inputBin):
+    inputStr = inputBin.decode('ascii')
+    return inputStr
+
+
+def stringToData(inputStr, dataType):
+    nodeNumber = inputStr[len(dataType): inputStr.find('=') - 1]
+    return {'dataType': dataType, 'data': inputStr[len(dataType + nodeNumber + ' = '):-2], 'nodeNumber': nodeNumber}
+
+
+def stringToDataHandler(inputStr):
+    dataTypes = ['SM', 'TS', 'T', 'H', 'FH', 'PH', 'WL', 'PhR', 'PS', 'LBS', 'G', 'MD', 'WM']
+
+    for dT in dataTypes:
+        if inputStr.startswith(dT):
+            return stringToData(inputStr, dT)
+
+    print("ERROR: Wrong data format.")
+
+
+def updateCurrentValuesOfVariables(inputData):
+    dataType = inputData['dataType']
+    nodeNumber = int(inputData['nodeNumber'])
+    data = float(inputData['data'])
+    if dataType in ['PS', 'LBS', 'TS']:  # Actuators -> return
+        data = int(inputData['data'])
+    currentValuesOfVariables[dataType][nodeNumber] = data
+
+
+def automateTasks(inputData):
+    dataType = inputData['dataType']
+
+    if dataType in ['PS', 'LBS', 'TS']:  # Actuators -> return
+        return
+    nodeNumber = int(inputData['nodeNumber'])
+    data = float(inputData['data'])
+
+    if dataType != 'SM':
+        average = 0
+        counter = 0
+        for V in currentValuesOfVariables[dataType]:
+            if V is not None:
+                average += V
+                counter += 1
+        if counter == 0:
+            return
+        average /= counter
+        data = average
+
+    if dataType == 'H':
+        if data < thresholdsOfSensors[dataType]['min'] and currentValuesOfVariables['PS'][2] == 0:
+            print('automateTasks: setPS2ON')
+            ser.write('setPS2ON\n'.encode('ascii'))
+        elif data > thresholdsOfSensors[dataType]['max'] and currentValuesOfVariables['PS'][2] == 1:
+            print('automateTasks: setPS2OFF')
+            ser.write('setPS2OFF\n'.encode('ascii'))
+
+    elif dataType == 'PhR':
+        if data < thresholdsOfSensors[dataType]['min'] and (currentValuesOfVariables['LBS'][0] == 0 or
+                                                            currentValuesOfVariables['LBS'][1] == 0):
+            print('automateTasks: setLBS0ON')
+            ser.write('setLBS0ON\n'.encode('ascii'))
+            print('automateTasks: setLBS1ON')
+            ser.write('setLBS1ON\n'.encode('ascii'))
+        elif data > thresholdsOfSensors[dataType]['max'] and (currentValuesOfVariables['LBS'][0] == 1 or
+                                                              currentValuesOfVariables['LBS'][1] == 1):
+            print('automateTasks: setLBS0OFF')
+            ser.write('setLBS0OFF\n'.encode('ascii'))
+            print('automateTasks: setLBS1OFF')
+            ser.write('setLBS1OFF\n'.encode('ascii'))
+
+    elif dataType == 'WL':
+        if data < thresholdsOfSensors[dataType]['min'] and currentValuesOfVariables['TS'][0] == 0:
+            print('automateTasks: setTS0ON')
+            ser.write('setTS0ON\n'.encode('ascii'))
+        elif data > thresholdsOfSensors[dataType]['max'] and currentValuesOfVariables['TS'][0] == 1:
+            print('automateTasks: setTS0OFF')
+            ser.write('setTS0OFF\n'.encode('ascii'))
+
+    elif dataType == 'SM':
+        if nodeNumber <= 7:
+            average = 0
+            counter = 0
+            for i in range(0, 8):
+                V = currentValuesOfVariables[dataType][i]
+                if V is not None:
+                    average += V
+                    counter += 1
+
+            if counter == 0:
+                return
+            average /= counter
+            data = average
+
+            if data < thresholdsOfSensors[dataType]['minV'] and currentValuesOfVariables['PS'][0] == 0:
+                print('automateTasks: setPS0ON')
+                ser.write('setPS0ON\n'.encode('ascii'))
+            elif data > thresholdsOfSensors[dataType]['maxV'] and currentValuesOfVariables['PS'][0] == 1:
+                print('automateTasks: setPS0OFF')
+                ser.write('setPS0OFF\n'.encode('ascii'))
+        else:
+            average = 0
+            counter = 0
+            for i in range(8, 18):
+                V = currentValuesOfVariables[dataType][i]
+                if V is not None:
+                    average += V
+                    counter += 1
+
+            if counter == 0:
+                return
+            average /= counter
+            data = average
+
+            if data < thresholdsOfSensors[dataType]['minF'] and currentValuesOfVariables['PS'][1] == 0:
+                print('automateTasks: setPS1ON')
+                ser.write('setPS1ON\n'.encode('ascii'))
+            elif data > thresholdsOfSensors[dataType]['maxF'] and currentValuesOfVariables['PS'][1] == 1:
+                print('automateTasks: setPS1OFF')
+                ser.write('setPS1OFF\n'.encode('ascii'))
+
+
+def sendData(inputData):
+    # try:
+    updateCurrentValuesOfVariables(inputData)
+    automateTasks(inputData)
+
+    payload = {'api_key': apiKeys[inputData["dataType"]][int(inputData["nodeNumber"])]['apiKey'],
+               'field1': inputData["data"]}
+    r = requests.post("http://thingtalk.ir/update", data=payload)
+    print("Data sent! ")
+    print(r.status_code, r.reason)
+    print("r.text: " + r.text)
+    print("--------------------------------------------------")
+    if r.status_code == 200 and r.text != '0':
+        return True
+    return False
+    # except:
+    #     print("Connection Error... Thingtalk is kidding us :(")
+    #     return False
+
+
+def serialReadThread():
+    print("Now Raspberry pi is ready :)")
+    while True:
+        if ser.inWaiting() > 0:
+            print(datetime.datetime.now())
+            inputBin = ser.readline()
+
+            inputStr = zigbeeDataToString(inputBin)
+            print("inputStr: " + inputStr)
+            inputData = stringToDataHandler(inputStr)
+            print("inputData: ")
+            print(inputData)
+            sendData(inputData)
+
+        ### TEST THREAD WORKING ###
+        # print("Enter 1 or 0:")
+        # num = input()
+        # ser.write(num.encode('ascii'))
+
 
 # GPIO.setmode(GPIO.BOARD)
 # pumpPin = 40
